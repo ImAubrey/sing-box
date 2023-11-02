2568ff3916cffadc99049cb88a75a62ea1b39478
Aubrey Yang <yblybl20152@gmail.com>
2023-11-03 00:19:57 +0800

    Update hysteria2_stub.go
    Signed-off-by: Aubrey Yang <yblybl20152@gmail.com>

--- inbound/hysteria2_stub.go
+++ inbound/hysteria2_stub.go
@@ -9,7 +9,7 @@ import (
 	"github.com/inazumav/sing-box/option"
 )
 
-func NewHysteria2(ctx context.Context, router adapter.Router, logger log.ContextLogger, tag string, options option.HysteriaInboundOptions) (adapter.Inbound, error) {
+func NewHysteria2(ctx context.Context, router adapter.Router, logger log.ContextLogger, tag string, options option.Hysteria2InboundOptions) (adapter.Inbound, error) {
 	return nil, C.ErrQUICNotIncluded
 }
 
